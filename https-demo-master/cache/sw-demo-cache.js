fe9bc193c0a2568591a073a14aed9d9a97f5cb27
郭腾 <victor.guo@siqibest.com>
2018-04-17 22:09:00 +0800

    1

--- https-demo-master/cache/sw-demo-cache.js
+++ https-demo-master/cache/sw-demo-cache.js
@@ -5,7 +5,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(VERSION).then(function(cache) {
       return cache.addAll([
-        './start.html',
+        // './start.html',
         './static/jquery.min.js',
         './static/mm1.jpg'
       ]);
